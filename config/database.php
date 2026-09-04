4bc1039286121e794d1b713c8ce71a908cccedbc
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-09-04 04:04:39 +0300

    fix db error

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
-         'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'url' => null,
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA')
                     ? base_path(env('MYSQL_ATTR_SSL_CA'))
                     : null,
                 PDO::ATTR_TIMEOUT => 10,
@@ -154,7 +154,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string)env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
